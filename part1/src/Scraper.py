094b2e4b143df7eb270eaa4fdef8b9b839a41d7c
Xancett <ryan.j@epoxstudios.com>
2022-01-24 18:28:39 -0500

    Setup for initial testing

--- part1/src/Scraper.py
+++ part1/src/Scraper.py
@@ -3,6 +3,10 @@ from requests.exceptions import RequestException
 from contextlib import closing
 from bs4 import BeautifulSoup
 
+url_list = [
+	'https://www.hookah-shisha.com/p-4246-starbuzz-hookah-tobacco-flavors-100.html'
+]
+
 # Function to get the URL content
 def simple_get(url):
 	# try to open the url
@@ -14,3 +18,13 @@ def simple_get(url):
 		log_error('Error during request')
 		return none
 
+# Function to get a list of all flavors
+def GetFlavors(request):
+	for i, li in enumerate(request.select('option')):
+		print(li.text)
+
+# Main
+for url in url_list:
+	pageRequest = simple_get(url)
+	page = BeautifulSoup(pageRequest, 'html.parser')
+	GetFlavors(page)
