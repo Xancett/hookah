aab7d7a9211e08e41b5fc236dd67adb89c177db4
Xancett <ryan.j@epoxstudios.com>
2022-02-08 09:27:05 -0500

    Built this in AWS/Lambda and copy/pasted when working

--- part2/src/shisha-api.py
+++ part2/src/shisha-api.py
@@ -1,14 +1,45 @@
+import boto3
 import json
 
-shisha_data = {}
+def lambda_handler(event, context):
+    return(Respond(event))
 
-# Gets the file and loads into shisha_data
-def GetFile():
-	# This will be updated later to read from the S3 bucket containing the json file
-	f = open('part2/flavors.json')
-	shisha_data = json.load(f)
-	print(shisha_data)
 
-# Main
-GetFile()
-#print(shisha_data)
+def Respond(res):
+    req = res['data']
+    requested = {}
+    if (req == 'brands'):
+        requested = GetBrands()
+    if (req == 'flavors'):
+        requested = GetFlavors(res['brand'])
+    return {
+        'statusCode' : 200,
+        'body' : json.dumps(requested),
+        'headers' : {
+            'Content-Type' : 'application/json',
+        },
+    }
+
+def GetBrands():
+    s3 = boto3.resource('s3')
+    content_object = s3.Object('shisha-db', 'flavors.json')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    returndata = {}
+    i = 0
+    for key in json_content.keys():
+        returndata[i] = key
+        i = i + 1
+    return returndata
+
+def GetFlavors(brand):
+    s3 = boto3.resource('s3')
+    content_object = s3.Object('shisha-db', 'flavors.json')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    returndata = {}
+    i = 0
+    for key in json_content[brand].keys():
+        returndata[key] = json_content[brand][key]
+        i = i + 1
+    return returndata
