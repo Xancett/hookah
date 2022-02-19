2494f4d4b870191c2727c90abc35f24163996c38
Xancett <ryan.j@epoxstudios.com>
2022-02-19 09:43:12 -0500

    API integration finally works

--- part3/public/home/assets/js/home.js
+++ part3/public/home/assets/js/home.js
@@ -28,4 +28,35 @@ document.addEventListener('click', e => {
 // Set the table to fill out from the API call
 async function UpdateTable(menuOption) {
 	console.log(menuOption);
+	let hookahAPI = 'https://er27enht4f.execute-api.us-east-1.amazonaws.com/default/fetch-shisha';
+	switch (menuOption) {
+		case 'All Flavors':
+			// Fetch all flavors from the API
+			let jsonData = { "data": "brands" };
+			const information = {
+				method: "POST",
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(jsonData)
+			};
+			// Send out information
+			let response = await fetch(hookahAPI, information);
+			// Wait for a response
+			let data = await response.json();
+			console.log(data);
+			jsonData['data'] = "flavors";
+			jsonData['brand'] = data[0];
+			information['body'] = JSON.stringify(jsonData);
+			response = await fetch(hookahAPI, information);
+			// Wait again
+			data = await response.json();
+			console.log(data);
+		case 'Enjoyed':
+			break;
+		case 'Plan to smoke':
+			break;
+		case 'Disliked':
+			break;
+	}
 }
