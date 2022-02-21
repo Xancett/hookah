f23235a28450f1838abf6722cd152e3bf3161b88
Xancett <ryan.j@epoxstudios.com>
2022-02-21 13:49:41 -0500

    Switched switch-case to if-else as it makes more sense, mock code works

--- part3/public/home/assets/js/home.js
+++ part3/public/home/assets/js/home.js
@@ -44,69 +44,77 @@ async function UpdateTable(menuOption) {
 	console.log(menuOption);
 	let hookahAPI = 'https://er27enht4f.execute-api.us-east-1.amazonaws.com/default/fetch-shisha';
 	let listData = {};
-	switch (menuOption) {
-		case 'All Flavors':
-			const information = {
-				method: "POST",
-				headers: {
-					'Content-Type': 'application/json'
-				},
-				body: ""
-			};	
-			// Check if brands is empty
-			if (Object.keys(brands).length == 0) {
-				// Fetch all flavors from the API
-				let jsonData = { "data": "brands" };
-				information['body'] = JSON.stringify(jsonData);
-				// Send out information
-				let response = await fetch(hookahAPI, information);
-				// Wait for a response
-				let data = await response.json();
-				brands = JSON.parse(JSON.stringify(data)); // Deep copy needed
-			}
-			if (brands['data'] == 'return') return;
-			let jsonData = { 'data': 'flavors', 'brand': brands[Object.keys(brands)[0]] };
+	if (menuOption == 'All Flavors') {
+		const information = {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: ""
+		};	
+		// Check if brands is empty
+		if (Object.keys(brands).length == 0) {
+			// Fetch all flavors from the API
+			let jsonData = { "data": "brands" };
 			information['body'] = JSON.stringify(jsonData);
-			response = await fetch(hookahAPI, information);
-			// Wait again
+			// Send out information
+			let response = await fetch(hookahAPI, information);
+			// Wait for a response
 			let data = await response.json();
-			const table = document.querySelectorAll('#tableOfContents');
-			for (var i = 0; i < Object.keys(data).length; i++) {
-				let row = document.createElement('tr');
-				let cell1 = document.createElement('td');
-				let cell2 = document.createElement('td');
-				let cell3 = document.createElement('td');
-				let cell4 = document.createElement('td');
-				let cell5 = document.createElement('td');
-				cell1.innerText = "Image missing";
-				cell2.innerText = jsonData['brand'];
-				cell3.innerText = Object.keys(data)[i];
-				cell4.innerText = "None";
-				cell5.innerText = "Not rated";
-				row.appendChild(cell1);
-				row.appendChild(cell2);
-				row.appendChild(cell3);
-				row.appendChild(cell4);
-				row.appendChild(cell5);
-				$("#tableOfContents").append(row);
-			}
-			delete brands[Object.keys(brands)[0]];
-			// Check if we have hit the end and shouldn't load anymore data
-			if (Object.keys(brands).length == 0) {
-				brands = { 'data': 'return' };
-			}
-		case 'Enjoyed':
-			listData = await GetList(menuOption);
-			for (var i = 0; i < listData['data'].length; i++) {
-				console.log("Brand: " + listData['data'][i].Brand);
-				console.log("Flavor: " + listData['data'][i].Flavor);
-				console.log("List: " + listData['data'][i].List);
-				console.log("Rating: " + listData['data'][i].Rating);
-			}
-		case 'Plan to smoke':
-			break;
-		case 'Disliked':
-			break;
+			brands = JSON.parse(JSON.stringify(data)); // Deep copy needed
+		}
+		if (brands['data'] == 'return') return;
+		let jsonData = { 'data': 'flavors', 'brand': brands[Object.keys(brands)[0]] };
+		information['body'] = JSON.stringify(jsonData);
+		response = await fetch(hookahAPI, information);
+		// Wait again
+		let data = await response.json();
+		const table = document.querySelectorAll('#tableOfContents');
+		for (var i = 0; i < Object.keys(data).length; i++) {
+			let row = document.createElement('tr');
+			let cell1 = document.createElement('td');
+			let cell2 = document.createElement('td');
+			let cell3 = document.createElement('td');
+			let cell4 = document.createElement('td');
+			let cell5 = document.createElement('td');
+			cell1.innerText = "Image missing";
+			cell2.innerText = jsonData['brand'];
+			cell3.innerText = Object.keys(data)[i];
+			cell4.innerText = "None";
+			cell5.innerText = "Not rated";
+			row.appendChild(cell1);
+			row.appendChild(cell2);
+			row.appendChild(cell3);
+			row.appendChild(cell4);
+			row.appendChild(cell5);
+			$("#tableOfContents").append(row);
+		}
+		delete brands[Object.keys(brands)[0]];
+		// Check if we have hit the end and shouldn't load anymore data
+		if (Object.keys(brands).length == 0) {
+			brands = { 'data': 'return' };
+		}
+	} else {
+		listData = await GetList(menuOption);
+		for (var i = 0; i < listData['data'].length; i++) {
+			let row = document.createElement('tr');
+			let cell1 = document.createElement('td');
+			let cell2 = document.createElement('td');
+			let cell3 = document.createElement('td');
+			let cell4 = document.createElement('td');
+			let cell5 = document.createElement('td');
+			cell1.innerText = "Image missing";
+			cell2.innerText = listData['data'][i].Brand;
+			cell3.innerText = listData['data'][i].Flavor;
+			cell4.innerText = listData['data'][i].List;
+			cell5.innerText = listData['data'][i].Rating;
+			row.appendChild(cell1);
+			row.appendChild(cell2);
+			row.appendChild(cell3);
+			row.appendChild(cell4);
+			row.appendChild(cell5);
+			$("#tableOfContents").append(row);
+		}
 	}
 }
 
