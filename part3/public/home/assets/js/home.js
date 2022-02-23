8918cc8d0fdc430a390cfd8b5835510d60d0fd51
Xancett <ryan.j@epoxstudios.com>
2022-02-23 11:47:33 -0500

    Restructured the event listener

--- part3/public/home/assets/js/home.js
+++ part3/public/home/assets/js/home.js
@@ -7,34 +7,34 @@ let brands = {};
 
 // Adds the event listener to the menu
 document.addEventListener('click', e => {
-	// Get the selected object
-	let origin = e.target.closest('a');
 	// Check if we should foward to optioncheck
 	if (e.target.closest('option') != null) {
 		OptionChange(e.target.closest('option'));
 		return;
 	}
-	// Check if we are trying to manipulate the labels for ratings
-	if (e.target.closest('label') != null) { console.log("Selecting rating") }
-	if (!origin) return; // No point in changing anything if what we clicked on is null
-	// Go through all selectable menu items and remove chosen if found
-	const menuOptions = document.querySelectorAll('#menu');
-	menuOptions.forEach(menu => {
-		if (menu.classList.contains("chosen")) {
-			// Check if we are just selecting the same menu
-			if (menu == origin) {
-				// Set the origin to null so nothing is updated
-				origin = null;
-				return; // So we don't remove 'chosen' from the currently selected menu
+	// Check if we are selecting menu items
+	if (e.target.closest('a') != null) {
+		// Get the selected object
+		let origin = e.target.closest('a');
+		// Go through all selectable menu items and remove chosen if found
+		const menuOptions = document.querySelectorAll('#menu');
+		menuOptions.forEach(menu => {
+			if (menu.classList.contains("chosen")) {
+				// Check if we are just selecting the same menu
+				if (menu == origin) {
+					// Set the origin to null so nothing is updated
+					origin = null;
+					return; // So we don't remove 'chosen' from the currently selected menu
+				}
+				menu.classList.remove("chosen");
 			}
-			menu.classList.remove("chosen");
+		});
+		// Change the selected object to be chosen
+		if (origin) {
+			origin.classList.add('chosen');
+			ClearTable();
+			UpdateTable(origin.text);
 		}
-	});
-	// Change the selected object to be chosen
-	if (origin) {
-		origin.classList.add('chosen');
-		ClearTable();
-		UpdateTable(origin.text);
 	}
 });
 
