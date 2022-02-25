c1c5338e1bc634dc90cccbf75347472b5fc9f010
Xancett <ryan.j@epoxstudios.com>
2022-02-25 13:46:02 -0500

    Created function for adding shisha to a list

--- part3/database.js
+++ part3/database.js
@@ -17,6 +17,26 @@ async function GetShisha(username, list) {
 		client.close();
 	}
 }
+// Adds an item to a list
+async function AddShisha(username, info) {
+	// Make sure info call is correct
+	if (info['Brand'] == null || info['Flavor'] == null || info['List'] == null || info['Rating'] == null) { return; }
+	const client = await MongoClient.connect("mongodb://localhost:27017/").catch(err => { console.log(err) });
+	if (!client) {
+		return;
+	}
+	try {
+		let myList = {};
+		myList[info['List']] = info;
+		let res = await client.db("shisha-saver").collection("users").updateOne({ "username": username }, { $push: myList });
+		console.log(res);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		client.close();
+	}
+}
 
 // Exports
 module.exports.GetShisha = GetShisha;
+module.exports.AddShisha = AddShisha;
