f31fec8001d027ff29dbd11559803b420887458d
Xancett <ryan.j@epoxstudios.com>
2022-02-25 16:08:56 -0500

    Added initial call for requesting list data

--- part3/webserver.js
+++ part3/webserver.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-//const MongoClient = require('mongodb').MongoClient;
+const Database = require('./database');
 
 
 // Setup app
 app.use(express.static(__dirname + '/public'));
+app.use(express.json({ limit: '1mb' }));
 app.listen(port, () => {
 	console.log("Listening...");
 });
@@ -13,5 +14,21 @@ app.listen(port, () => {
 
 // Home page
 app.get('/', (request, response) => {
+	//Database.HardReset();
+	//Database.UpdateList('test', {"Brand": "Fumari", "Flavor": "Mint", "List": "Enjoyed", "Rating": "4"});
+	//Database.AddShisha('test', {"Brand": "Fumari", "Flavor": "Mint", "List": "Enjoyed", "Rating": "4"});
+	//Database.GetShisha('test', 'Enjoyed').then(response => console.log(response));
+	//Database.UpdateList('test', [{ "Brand": "Starbuzz", "Flavor": "Black Grape", "List": "Enjoyed", "Rating": "3" }, { "Brand": "Starbuzz", "Flavor": "Candy", "List": "Enjoyed", "Rating": "5" }, { "Brand": "Ugly Shisha", "Flavor": "Hurricane", "List": "Enjoyed", "Rating": "4" }, { "Brand": "Fumari", "Flavor": "Sour Cherry", "List": "Enjoyed", "Rating": "3" } ]);
+	//Database.SeekAndDestroy('test', {"Brand": "Fumari", "Flavor": "Mint", "List": "Enjoyed", "Rating": "4"});
 	response.sendFile(__dirname + "/public/home.html");
+});
+
+// List request
+app.post('/hookahapi', (request, response) => {
+	// Check to ensure the request is proper
+	if (request.body['inforequest'] == null) { response.close(); }
+	Database.GetShisha('test', request.body.inforequest).then(res => {
+		//console.log(request.body.inforequest);
+		response.send(res);
+	});
 });
