ea43a6ba08112bb4dd10d4cdbc0a92ec05512e8c
Xancett <ryan.j@epoxstudios.com>
2022-03-02 11:47:46 -0500

    Added page for creating an account

---
+++ part3/public/create/assets/js/create.js
@@ -0,0 +1,82 @@
+
+// Event listener for the submit button
+window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+		form.addEventListener('submit', function(event) {
+			if (form.checkValidity() === false) {
+				console.log("Form not validated");
+				//event.preventDefault();
+          		//event.stopPropagation();
+			} else {
+				console.log("Start spinning wheel");
+				LoadingData(true);
+				event.preventDefault();
+				LogIn(form[0].value, form[1].value);
+			}
+    	}, false);
+	});
+}, false);
+
+// Sets information in the table on if the table is being loaded or not with data
+function LoadingData(loading) {
+	if (loading) {
+		$("body").addClass("loading");
+	} else {
+		$("body").removeClass("loading");
+	}
+}
+
+// Starts the call for logging in
+async function LogIn(username, password) {
+	let token = "";
+	if (GetCookie() != "" && GetCookie() != undefined) {
+		token = GetCookie();
+		PageRedirect(token);
+	}
+	try {
+		// Setup request
+		const jsonData = { 'username': username, 'password': password };
+		const information = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(jsonData)
+		};
+		const response = await fetch('/shishalogintoken', information);
+		let data = await response.json();
+		const d = new Date();
+		d.setTime(d.getTime() + 86400000);
+		document.cookie = "SecurityToken=" + data['SecurityToken'] + "; SameSite=Strict;" + "expires=" + d.toUTCString() + ";path=/";
+		token = data['SecurityToken'];
+		LoadingData(false);
+		PageRedirect(token);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function PageRedirect(token) {
+	try {
+		// Setup request
+		const information = {
+			method: 'GET',
+			headers: { 'Content-Type': 'application/json', 'SecurityToken': token }
+		};
+		const response = await fetch('/', information);
+		window.location.href = response.url;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+function GetCookie() {
+	let st = "SecurityToken=";
+	let ca = document.cookie.split(';');
+	for (let i = 0; i < ca.length; i++) {
+		if (ca[i].includes(st)) {
+			return ca[i].substring(ca[i].indexOf(st) + 14);
+		}
+	}
+}
